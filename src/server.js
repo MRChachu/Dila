ccae22de67963ac0d281757650f60019d59a487a
Chachu <g.chachu63@gmail.com>
2026-07-05 21:31:24 +0400

    Added Online Players and Invite System

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://dila-alpha.vercel.app'],
+  origin: ['http://localhost:5173', 'https://dila-alpha.vercel.app', 'https://phurti.ge', 'https://www.phurti.ge'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
@@ -41,7 +41,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 const io = new Server(server, {
   cors: { 
-    origin: ['http://localhost:5173', 'https://dila-alpha.vercel.app'], 
+    origin: ['http://localhost:5173', 'https://dila-alpha.vercel.app', 'https://phurti.ge', 'https://www.phurti.ge'], 
     methods: ["GET", "POST"],
     credentials: true
   }
